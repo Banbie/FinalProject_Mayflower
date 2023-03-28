255afa282c6fd67340de52892de7f5304f66c8f3
blessis <rhdwn9907@gmail.com>
2023-03-28 10:12:52 +0900

    tab_device

--- FinalProject_C3/Tab_Device.cs
+++ FinalProject_C3/Tab_Device.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using LoginTest;
 using MySql.Data.MySqlClient;
+using Org.BouncyCastle.Asn1.X509;
 
 namespace FinalProject_C3
 {
@@ -29,43 +30,7 @@ namespace FinalProject_C3
             getcb_pro();
             timer1.Start();
         }
-
-        //private void getcb_prod()
-        //{
-        //    cb_prod.Items.Clear();
-        //    cb_prod.Items.Add("제품번호");
-        //    try
-        //    {
-        //        string strqry = "select prodnum from tb_prod";
-        //        foreach (DataRow dr in db.Select(strqry).Rows)
-        //        {
-        //            cb_prod.Items.Add(dr["prodnum"]);
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show(ex.Message);
-        //    }
-        //}
-
-        //private void get_usernum()
-        //{
-        //    string con = "usernum";
-        //    string tb = "tb_user";
-        //    string where = $"where username = '{lb_name.Text}'";
-        //    lb_usernum.Text = db.SelectDetail(con, tb, where).Tables[0].Rows[0][0].ToString();
-        //}
-
-        //private void get_cur()
-        //{
-        //    string tableOrder= $"tb_cur order by curnum DESC LIMIT 1";
-        //    dgv_cur.DataSource = db.SelectAll(tableOrder).Tables[0];
-        //}
-
-        //private void timer1_Tick(object sender, EventArgs e)
-        //{
-        //    dgv_device.DataSource = db.SelectAll("tb_flow").Tables[0];
-        //} 
+ 
 
         private void bt_flowupdate_Click(object sender, EventArgs e)
         {
@@ -167,47 +132,25 @@ namespace FinalProject_C3
             DateTime sDate = dt_start.Value.Date;
             DateTime eDate = dt_end.Value.Date.AddDays(1);
 
-            // 총 생산량 쿼리 실행
-            string query = $"SELECT count(prodnum) FROM tb_flow WHERE pr = '완료' AND DATE(donetime) >= '{startDate:yyyy-MM-dd}' AND DATE(donetime) < '{endDate:yyyy-MM-dd}'";
-            //try
-            //{
-            //    object result = db.ExecuteScalar(query);
-            //    int totalQuantity = Convert.ToInt32(result ?? 0);
-            //    int totalDefectCount = 0;
+            string cond = "d.devicename, d.proname, f.flowdate, count(*)";
+            string table = "tb_flow f left join tb_device d on f.pronum = d.pronum";
+            string where;
 
-            //    // 불량품 수 쿼리 실행
-            //    string defectQuery = $"SELECT DATE(proddate), COUNT(*) FROM tb_prod WHERE DATE(proddate) >= '{startDate:yyyy-MM-dd}' AND DATE(proddate) < '{endDate:yyyy-MM-dd}' AND spec = '1' GROUP BY DATE(proddate)";
-            //    DataTable defectTable = db.ExecuteDataTable(defectQuery);
+            if (cb_pro.SelectedItem != "전체")
+            {   where = $"WHERE proname = {cb_pro.SelectedItem} AND DATE(flowdate) >= '{sDate:yyyy-MM-dd}' AND DATE(flowdate) < '{eDate:yyyy-MM-dd}'";  }
+            else
+            {   where = $"WHERE DATE(flowdate) >= '{sDate:yyyy-MM-dd}' AND DATE(flowdate) < '{eDate:yyyy-MM-dd}'";  }
 
-            //    // 검색 결과를 출력할 데이터 그리드뷰의 컬럼 설정
-            //    dgv_Search.Columns.Clear();
-            //    dgv_Search.Columns.Add("date", "날짜");
-            //    dgv_Search.Columns.Add("totalQuantity", "총 생산량");
-            //    dgv_Search.Columns.Add("defectCount", "불량 갯수");
-            //    dgv_Search.Columns.Add("defectRate", "불량률(%)");
+            string groupBy = " group by mayflower.d.devicename, mayflower.d.proname, mayflower.f.flowdate;";
 
-            //    // 각 일자별 생산량과 불량품 수, 불량률 계산 및 데이터 그리드뷰에 출력
-            //    foreach (DataRow row in defectTable.Rows)
-            //    {
-            //        DateTime date = Convert.ToDateTime(row["DATE(proddate)"]);
-            //        int dailyQuantity = GetDailyQuantity(date, startDate, endDate);
-            //        int count = Convert.ToInt32(row["COUNT(*)"]);
-            //        double defectRate = dailyQuantity != 0 ? Math.Round((double)count / dailyQuantity * 100, 2) : 0;
-            //        totalDefectCount += count;
-
-            //        dgv_Search.Rows.Add(date.ToString("yyyy-MM-dd"), dailyQuantity, count, $"{defectRate}%");
-            //    }
-
-            //    // 총 생산량과 불량률 계산 및 출력
-            //    double totalDefectRate = totalQuantity != 0 ? Math.Round((double)totalDefectCount / totalQuantity * 100, 2) : 0;
-            //    dgv_Total.Rows.Clear();
-            //    dgv_Total.Rows.Add($"{startDate:yyyy-MM-dd} ~ {endDate.AddDays(-1):MM-dd}", totalQuantity, totalDefectCount, $"{totalDefectRate}%");
-
-            //}
-            //catch (MySqlException ex)
-            //{
-            //    MessageBox.Show(ex.Message);
-            //}
+            try
+            {
+                dgv_deflow.DataSource = db.SelectDetail(cond, table, where + groupBy).Tables[0];
+            }
+            catch (MySqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
